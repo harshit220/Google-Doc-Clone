ea7c567742c2caeda5f42d85df9215de25130ef5
Harshit Sachan <harshitsachan99@gmail.com>
2023-05-18 20:29:25 +0530

    updated

--- source/src/App.js
+++ source/src/App.js
@@ -6,11 +6,9 @@ function App() {
     <div className="App">
       <Doc />
       <div class="clear"></div>
-
       <div class="page">
         <p style={{color:"gray",margin:"40px 40px"}}><i>Type @ to insert</i></p>
       </div>
-
       <div class="page2"></div>
     </div>
   );
--- source/src/Components/Doc.jsx
+++ source/src/Components/Doc.jsx
@@ -7,7 +7,7 @@ export const Doc = () => {
         <img
           src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724c0e4b0c296de94c833/1441211585166/bluedoclogo.png?"
           className="docsLogo"
-          alt=""
+          alt="i"
         />
         <h1 id="untitled">
           <em>Untitled document</em>
@@ -29,7 +29,7 @@ export const Doc = () => {
             <img
               id="emailcaret"
               src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724bde4b076794acc4218/1441211581744/caret.png?format=300w"
-              alt=""
+              alt="img"
             />
           </span>
 
@@ -43,29 +43,29 @@ export const Doc = () => {
           <img
             class="toolbarButtonsPrint"
             src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724c0e4b076794acc425b/1441211584574/printer.png"
-            alt=""
+            alt="img"
           />
           <img
             class="toolbarButtons"
             src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724c0e4b076794acc4259/1441211584347/back.png"
-            alt=""
+            alt="img"
           />
           <img
             class="toolbarButtons"
             src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724c0e4b076bc359bf9f3/1441211584386/fwd.png"
-            alt=""
+            alt="img"
           />
           <img
             class="toolbarButtonsPaint"
             src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724c0e4b076794acc4257/1441211584157/paint.png"
-            alt=""
+            alt="img"
           />
           <span class="toolbarButtons100">
             100%
             <img
               id="hunCaret"
               src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724bde4b076794acc4218/1441211581744/caret.png?format=300w"
-              alt=""
+              alt="img"
             />
           </span>
           <span class="toolbarButtonsNormal">
@@ -73,7 +73,7 @@ export const Doc = () => {
             <img
               id="normalCaret"
               src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724bde4b076794acc4218/1441211581744/caret.png?format=300w"
-              alt=""
+              alt="img"
             />
           </span>
           <span class="toolbarButtonsArial">
@@ -89,58 +89,58 @@ export const Doc = () => {
             <img
               id="twelveCaret"
               src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724bde4b076794acc4218/1441211581744/caret.png?format=300w"
-              alt=""
+              alt="img"
             />
           </span>
           <img
             class="toolbarButtonsB"
             src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724bfe4b076794acc4251/1441211583548/bold.png?format=300w"
-            alt=""
+            alt="img"
           />
           <img
             class="toolbarButtonsI"
             src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724bfe4b076bc359bf9e6/1441211583336/i.png?format=300w"
-            alt=""
+            alt="img"
           />
           <img
             class="toolbarButtonsU"
             src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724bfe4b076bc359bf9ea/1441211583819/under.png?format=300w"
-            alt=""
+            alt="img"
           />
           <img
             class="toolbarButtonsColor"
             src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724bfe4b076bc359bf9e8/1441211583603/color.png?format=300w"
-            alt=""
+            alt="img"
           />
           <img
             class="toolbarButtonsHyper"
             src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724bfe4b076794acc4253/1441211583862/hyper.png?format=300w"
-            alt=""
+            alt="img"
           />
           <img
             class="toolbarButtonsMsg"
             src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724bfe4b076794acc424f/1441211583411/msg.png?format=300w"
-            alt=""
+            alt="img"
           />
           <img
             class="toolbarButtons"
             src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724bfe4b076794acc4255/1441211584087/left.png?format=300w"
-            alt=""
+            alt="img"
           />
           <img
             class="toolbarButtons"
             src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724c0e4b076bc359bf9ef/1441211585710/center.png?format=300w"
-            alt=""
+            alt="img"
           />
           <img
             class="toolbarButtons"
             src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724bfe4b076bc359bf9ed/1441211583977/right.png?format=300w"
-            alt=""
+            alt="img"
           />
           <img
             class="toolbarButtonsJust"
             src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724bfe4b076bc359bf9e4/1441211583110/just.png?format=300w"
-            alt=""
+            alt="img"
           />
           <img
             class="toolbarButtonsLine"
@@ -150,29 +150,28 @@ export const Doc = () => {
           <img
             class="toolbarButtonsNumbered"
             src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724bee4b076794acc4246/1441211582789/numbered.png?format=300w"
-            alt=""
+            alt="img"
           />
           <img
             class="toolbarButtonsBullet"
             src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724bee4b076794acc424b/1441211582989/bullet.png?format=300w"
-            alt=""
+            alt="img"
           />
           <img
             class="toolbarButtonsIndentLeft"
             src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724bee4b076794acc421f/1441211582304/indentright.png?format=300w"
-            alt=""
+            alt="img"
           />
           <img
             class="toolbarButtonsIndentRight"
             src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724bee4b076794acc4221/1441211582457/indentleft.png?format=300w"
-            alt=""
+            alt="img"
           />
           <img
             class="toolbarButtonsClear"
             src="http://static1.squarespace.com/static/55dcfb03e4b07360409d0471/55e724bde4b0c296de94c811/55e724bee4b076794acc421d/1441211582124/clear.png?format=300w"
-            alt=""
-          />
-         
+            alt="img"
+          />  
         </div>
         <div class="slider">
           <img src="" alt="" />
